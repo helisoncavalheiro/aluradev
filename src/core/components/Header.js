b9c0d3acd8ab05aac36407d9a1e6be39f3c844fc
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-05-18 10:39:51 -0300

    Adiciona a barra de pesquisa e o menu hamburguer

--- src/core/components/Header.js
+++ src/core/components/Header.js
@@ -1,13 +1,23 @@
-import logo from '../../assets/img/logo.png';
+import logo from "../../assets/img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
 
-const Header = ()=>{
-	return (
-		<header className="header">
-			<div className="container">
-				<img src={logo} className="img-responsive" alt="Logo"></img>
-			</div>
-		</header>
-	)
-}
+const Header = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="logo">
+          <img src={logo} className="img-responsive" alt="Logo"></img>
+        </div>
+        <div className="search">
+          <input className="search-bar" placeholder="Busque por algo" />
+        </div>
+        <div className="menu-icon">
+          <FontAwesomeIcon icon={faBars} size="lg" />
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
