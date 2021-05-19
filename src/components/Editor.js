e46fa9f3fb37bc050398c1b4c23cba82f37a4691
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-05-18 23:11:16 -0300

    Cria o Editor de código

---
+++ src/components/Editor.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import editor from "../css/editor.css";
+import MacNav from "./MacNav";
+export default function Editor() {
+  const [content, setContent] = useState(
+    `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
+const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
+    
+const unfold = (f, seed) => {
+  const go = (f, seed, acc) => {
+  const res = f(seed)
+  return res ? go(f, res[1], acc.concat([res[0]])) : acc
+}
+  return go(f, seed, [])
+}`
+  );
+
+  return (
+    <div className="editor editor-green">
+      <div className="editor-wraper">
+        <MacNav />
+        <textarea
+          className="editor-content"
+          value={content}
+          onChange={(evt) => setContent(evt.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
