ed91591909b48533ba5be9fe6657e4eec2aa91ee
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-05-18 22:45:33 -0300

    Cria os botões de navegação do Mac

---
+++ src/components/MacNav.js
@@ -0,0 +1,12 @@
+import macnav from '../css/macnav.css';
+const MacNav = () => {
+  return (
+    <div className="mac-nav">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default MacNav;
