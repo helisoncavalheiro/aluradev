aa25ad8f5f672a7883e3f42f0476cb7169844cb1
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-05-19 16:10:02 -0300

    Implementa o botão de highlight

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./css/index.css";
+import "./css/buttons.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 
@@ -9,11 +10,18 @@ function App() {
         <Header />
       </div>
       <div className="container">
-        <div className="box box-lg-25"></div>
-        <div className="box box-lg-50 box-sm-100">
-          <Editor />
+        <div className="main">
+          <div className="box box-lg-25">
+            <div style={{height: '100%'}}></div>
+          </div>
+          <div className="box box-lg-50 box-sm-100">
+            <Editor />
+            <div className="container">
+              <button className="btn btn-dark full-width">Visualizar com highlight</button>
+            </div>
+          </div>
+          <div className="box box-lg-25"></div>
         </div>
-        <div className="box box-lg-25"></div>
       </div>
     </>
   );
